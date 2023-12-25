08553935362687e3083c580edd660b0a89e40f46
Sonic7911 <sonicgautam111@gmail.com>
2023-12-25 20:45:54 +0545

    Navbar

--- hackanthonproj/src/components/Home.js
+++ hackanthonproj/src/components/Home.js
@@ -6,9 +6,9 @@ const Home = () => {
   console.log(a);
   return (
     <>
-      <div className="h-screen w-screen flex justify-around  ">
-        <div className="  w-1/2 h-20 flex justify-start items-center pl-8">Logo</div>
-        <div className="  w-1/2 h-20 flex justify-center gap-10 items-center">
+      <div className="w-screen flex justify-between items-center px-6 py-6">
+        <div className="flex items-center">Logo</div>
+        <div className="flex gap-10">
           <div>Home</div>
           <div>Near me</div>
           <div>Advisor</div>
