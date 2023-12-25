7b4d3a694d237920db98661c6ea259bf60f92c0e
Sonic7911 <sonicgautam111@gmail.com>
2023-12-25 20:30:02 +0545

    Navbar

--- hackanthonproj/src/components/Home.js
+++ hackanthonproj/src/components/Home.js
@@ -1,12 +1,21 @@
-import React, { useContext } from 'react'
-import context from '../context/maincontext'
+import React, { useContext } from "react";
+import context from "../context/maincontext";
 
 const Home = () => {
-    const a=useContext(context);
-    console.log(a)
+  const a = useContext(context);
+  console.log(a);
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="h-screen w-screen flex justify-around  ">
+        <div className="  w-1/2 h-20 flex justify-start items-center pl-8">Logo</div>
+        <div className="  w-1/2 h-20 flex justify-center gap-10 items-center">
+          <div>Home</div>
+          <div>Near me</div>
+          <div>Advisor</div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
--- hackanthonproj/src/utils/index.js
+++ hackanthonproj/src/utils/index.js
@@ -1,20 +1,22 @@
 import Login from "../components/Login";
 import Home from "../components/Home";
 import { NearMe } from "../components/NearMe";
-const URL =[{
-name:'Home',
-element:<Home/>,
-path:"/"
-},
-{
-    name: 'Login',
-    element:<Login/>,
-    path:"/login"
-},
-{
-    name:'Near',
-    element:<NearMe/>,
-    path:"/nearme"
-}]
+const URL = [
+  {
+    name: "Home",
+    element: <Home />,
+    path: "/",
+  },
+  {
+    name: "Login",
+    element: <Login />,
+    path: "/login",
+  },
+  {
+    name: "Near",
+    element: <NearMe />,
+    path: "/nearme",
+  },
+];
 
 export default URL;
